0c2e6ddf7323ea7b979c321b31b9dda540576b97
Tshianeo28 <nevhufumbatp@gmail.com>
2020-06-05 16:29:15 +0200

    enables users to logout

---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php
+$hostname="localhost";
+$hostusername="thaboirv_root";
+$hostpassword="2019@Melbroks";
+$dbname="thaboirv_CT";
+
+
+$con = mysqli_connect($hostname,$hostusername,$hostpassword,$dbname);
+
+?>
